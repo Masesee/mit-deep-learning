b0872dc2745ab32b2b9736e182704be6bab1338f
Enoch Masese <maseseenoch@gmail.com>
2025-03-27 11:19:20 +0300

    Initial implementation of custom dense layer and multi-output perceptron examples in TensorFlow and PyTorch

--- 1. Introduction to Deep Learning/layers_architecture.py
+++ 1. Introduction to Deep Learning/layers_architecture.py
@@ -43,9 +43,9 @@ model = nn.Sequential(
 import tensorflow as tf
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(n1)
-    tf.keras.layers.Dense(n2)
-    ...
+    tf.keras.layers.Dense(n1),
+    tf.keras.layers.Dense(n2),
+    # Add more layers as needed
     tf.keras.layers.Dense(2)
 ])
 
